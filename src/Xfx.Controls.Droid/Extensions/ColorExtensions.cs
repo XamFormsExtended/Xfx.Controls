e10a0a736859af57c52d1ee7bd1ede65c55d865e
Chase Florell <chase.florell@gmail.com>
2017-03-10 12:10:07 -0800

    [CF] initial changes to get going

---
+++ src/Xfx.Controls.Droid/Extensions/ColorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Util;
+
+namespace Xfx.Controls.Droid.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color GetColorByResourceId(this Context context, int resId)
+        {
+            var themeAccentColor = new TypedValue();
+            context.Theme.ResolveAttribute(resId, themeAccentColor, true);
+            return new Color(themeAccentColor.Data);
+        }
+    }
+}
