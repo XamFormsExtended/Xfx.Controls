d25128ccc7fa28f6c2ce5185753e91235dbd3031
vasil kanev <vasilkynev@abv.bg>
2018-05-30 23:10:36 +0300

    Added feature. Now on item selected event returns also the selected index

---
+++ src/Xfx.Controls/XfxSelectedItemChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Xfx
+{
+    public class XfxSelectedItemChangedEventArgs : SelectedItemChangedEventArgs
+    {
+        public int SelectedItemIndex { get; private set; }
+
+        public XfxSelectedItemChangedEventArgs(object selectedItem, int selectedIndex) : base(selectedItem)
+        {
+            SelectedItemIndex = selectedIndex;
+        }
+    }
+}
